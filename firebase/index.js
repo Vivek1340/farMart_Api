ce2a493919ff9180506326cfc2f0e967d147efd0
Vivek1340 <vivekanand1340@gmail.com>
2023-10-08 20:50:16 +0530

    minor changes

--- firebase/index.js
+++ firebase/index.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import utils from '../utils.js'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlAsfxCAKLQ35rR3eSpCmo3ySz13Yqfws",
+  apiKey: utils.API_KEY,
   authDomain: "farmart-d4c15.firebaseapp.com",
   projectId: "farmart-d4c15",
   storageBucket: "farmart-d4c15.appspot.com",
   messagingSenderId: "106480352433",
-  appId: "1:106480352433:web:cf0d36fb6184fe95e03690",
+  appId: utils.APP_ID,
   measurementId: "G-X4Q800LQ4H",
 };
 let app = initializeApp(firebaseConfig);
--- utils.js
+++ utils.js
@@ -5,4 +5,6 @@ export default {
   MONGO_CONNECTION_URL: process.env.MONGO_URL,
   PORT: process.env.PORT || 8000,
   JWT_SECRET: process.env.JWT_SECRET,
+  API_KEY: process.env.API_KEY,
+  APP_ID: process.env.APP_ID
 };
